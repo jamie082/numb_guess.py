9d1636bb8f39e4c0684a2242b714c6a912ae35cd
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-10-07 21:11:30 -0700

    fixed working source

--- numb_guess.py
+++ numb_guess.py
@@ -4,3 +4,17 @@ print ("Hello World! Welcome to the game!")
 time.sleep(3)
 n = random.randint(1, 10)
 print("Number has been generated!!\nYou have 4 chances to guess the number!")
+
+count = 4
+
+while count != 0:
+    a = int(input("Guess the number: "))
+
+    if a == n:
+        print("Correct! You got the number right!")
+        break
+    elif a > n:
+        print("lower ", a)
+    else:
+        print("Greater ", a)
+    count =- 1

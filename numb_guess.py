2717766d9b65f94c90e6d83365abfd6c108b266f
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-10-07 21:04:03 -0700

    added files

---
+++ numb_guess.py
@@ -0,0 +1,6 @@
+import random
+import time
+print ("Hello World! Welcome to the game!")
+time.sleep(3)
+n = random.randint(1, 10)
+print("Number has been generated!!\nYou have 4 chances to guess the number!")
